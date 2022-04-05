20fbf92deaba7ff5af1fa51c35013cbcae0ee84e
mhborthwick <mhborthwick@gmail.com>
2022-04-04 19:02:02 -0700

    chore: setup jest config

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+// https://nextjs.org/docs/testing#setting-up-jest-with-the-rust-compiler
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleDirectories: ["node_modules", "<rootDir>/"],
+  testEnvironment: "jest-environment-jsdom",
+};
+
+module.exports = createJestConfig(customJestConfig);
