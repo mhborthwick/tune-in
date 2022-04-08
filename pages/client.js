98ebb6391c3d6b08bcaf851c2acb20fbfe37dbb8
mhborthwick <mhborthwick@gmail.com>
2022-04-07 20:14:06 -0700

    feat: add ability to search tracks

--- pages/client.js
+++ pages/client.js
@@ -1,21 +1,27 @@
 import Layout from "../components/layout";
 import { getItemFromStorage, getTokens } from "../utils/authUtils";
+import { useRef } from "react";
+import { search } from "../lib/spotify";
 
 // Temp page to test Spotify API functionality
 // Make sure to access this from the front page
 // by clicking Log In to Get Started
 export default function GetData() {
+  const tokensRef = useRef({ access_token: "", refresh_token: "" });
   return (
     <Layout>
       <button
         onClick={async () => {
           const code = getItemFromStorage("code");
           const verifier = getItemFromStorage("verifier");
-          const tokens = await getTokens(code, verifier);
-          console.log(tokens);
+          const tokens = await getTokens(code, verifier, tokensRef);
+          tokensRef.current.access_token = tokens.access_token;
+          tokensRef.current.refresh_token = tokens.refresh_token;
+          const data = await search(tokensRef.current.access_token);
+          console.log(data);
         }}
       >
-        Get Token
+        Search Track
       </button>
     </Layout>
   );
