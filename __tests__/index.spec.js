b6bc016a67e12ba1cf413ef836945a8365c5f6eb
Mike Borthwick <41981940+mhborthwick@users.noreply.github.com>
2022-04-09 20:26:40 -0700

    chore: clean up auth file (#17)

--- __tests__/index.spec.js
+++ __tests__/index.spec.js
@@ -1,25 +1,25 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import * as uuid from "uuid";
 import Home from "../pages/index";
-import * as authUtil from "../utils/authUtils";
+import * as auth from "../utils/auth";
 
 jest.mock("uuid");
 
 describe("Home", () => {
   it("renders a button that triggers redirect with query on click", () => {
-    jest.spyOn(authUtil, "redirectToAuthUrl").mockImplementationOnce(jest.fn());
+    jest.spyOn(auth, "redirectToAuthUrl").mockImplementationOnce(jest.fn());
     jest.spyOn(uuid, "v4").mockReturnValueOnce("123abcABC");
     const values = { verifier: "foo", challenge: "bar" };
-    jest.spyOn(authUtil, "getCodeChallenge").mockReturnValueOnce(values);
+    jest.spyOn(auth, "getCodeChallenge").mockReturnValueOnce(values);
     render(<Home />);
     const button = screen.getByRole("button", {
       name: /Log in to Get Started/i,
     });
     expect(button).toBeInTheDocument();
     fireEvent.click(button);
-    expect(authUtil.redirectToAuthUrl).toHaveBeenCalledTimes(1);
+    expect(auth.redirectToAuthUrl).toHaveBeenCalledTimes(1);
     const mockQuery =
       "response_type=code&client_id=foo&scope=user-read-private+user-read-email&redirect_uri=bar&state=123abcABC&code_challenge_method=S256&code_challenge=bar";
-    expect(authUtil.redirectToAuthUrl).toHaveBeenCalledWith(mockQuery);
+    expect(auth.redirectToAuthUrl).toHaveBeenCalledWith(mockQuery);
   });
 });
