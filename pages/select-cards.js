f176213f802f9aa4ecf2c3141496e4090234d58f
Mike Borthwick <41981940+mhborthwick@users.noreply.github.com>
2022-04-24 18:12:06 -0700

    feat: start to add cards (#32)

--- pages/select-cards.js
+++ pages/select-cards.js
@@ -1,9 +1,23 @@
 import Layout from "../components/layout";
+import { Cards } from "../components/card";
+import { useState } from "react";
 
 export default function SelectCards() {
+  const [count, updateCount] = useState(0);
+  const [cardState, setCardState] = useState({
+    danceability: 0,
+    energy: 0,
+    loudness: 0,
+    popularity: 0,
+  });
+  console.log(cardState, count);
   return (
     <Layout>
-      <div>hi</div>
+      {count === 3 ? (
+        <div>results</div>
+      ) : (
+        <Cards setCardState={setCardState} updateCount={updateCount} />
+      )}
     </Layout>
   );
 }
