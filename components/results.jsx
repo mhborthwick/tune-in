4aa00fb3ddfa485d95faa2d8c3c79616fafbbc0e
Mike Borthwick <41981940+mhborthwick@users.noreply.github.com>
2022-05-01 11:23:12 -0700

    feat: add playlist (#34)

--- components/results.jsx
+++ components/results.jsx
@@ -5,9 +5,31 @@ import { Spotify } from "../lib/spotify";
 export const Results = ({ cardState }) => {
   const tokens = useRef({ access_token: "", refresh_token: "" });
   const client = Spotify.init(tokens);
+  const getUserId = async () => {
+    const response = await client.getUserId();
+    const json = await response.json();
+    return json.id;
+  };
+  const getPlaylistId = async (data) => {
+    const response = await client.createPlaylist(data);
+    const json = await response.json();
+    return json.id;
+  };
   const fetcher = async (_, params) => {
-    const response = await client.getRecommendations(params);
-    return await response.json();
+    const recResponse = await client.getRecommendations(params);
+    const recJson = await recResponse.json();
+    const { tracks } = recJson;
+    const uris = tracks.map((t) => {
+      return t.uri;
+    });
+    const id = await getUserId();
+    const playlistData = { id: id, name: "test", description: "hello" };
+    const playlistId = await getPlaylistId(playlistData);
+    const data = { id: playlistId, uris: uris };
+    console.log(data);
+    const response = await client.addToPlaylist(data);
+    const json = await response.json();
+    return json;
   };
   const params = {
     seedArtists: "3XxNRirzbjfLdDli06zMaB,",
@@ -19,6 +41,8 @@ export const Results = ({ cardState }) => {
     targetPopularity: cardState.popularity,
   };
   const { data } = useSWR(["getTracks", params], fetcher, {
+    revalidateIfStale: false,
+    shouldRetryOnError: false,
     revalidateOnFocus: false,
     revalidateOnReconnect: false,
     refreshWhenOffline: false,
@@ -28,6 +52,6 @@ export const Results = ({ cardState }) => {
   if (!data) {
     return <div>Loading...</div>;
   } else if (data) {
-    return <div>{data.tracks[0].name}</div>;
+    return <div>playlist created</div>;
   }
 };
