ccff57298abae031f06d2e300bc25e7aa47c6fd2
Mike Borthwick <41981940+mhborthwick@users.noreply.github.com>
2022-06-01 23:02:48 -0700

    Mhborthwick/fix spotify client test (#44)

--- __tests__/spotify.spec.js
+++ __tests__/spotify.spec.js
@@ -2,31 +2,17 @@ import { Spotify } from "../lib/spotify";
 
 describe("Class: Spotify", () => {
   describe("Method: init", () => {
-    it("should throw if mutable ref object is not provided", () => {
-      const notMutableRefObj = {
-        access_token: "foo",
-        refresh_token: "bar",
-      };
-      expect(() => Spotify.init(notMutableRefObj)).toThrow();
-    });
-
-    it("should create new instance of Spotify class with tokens", () => {
-      const mockMutableRefObj = {
-        current: {
-          access_token: "foo",
-          refresh_token: "bar",
-        },
-      };
-      const client = Spotify.init(mockMutableRefObj);
+    it("should create new instance of Spotify class", () => {
+      const client = Spotify.init();
       expect(client).toBeInstanceOf(Spotify);
-      expect(client.tokens.current.access_token).toEqual("foo");
-      expect(client.tokens.current.refresh_token).toEqual("bar");
     });
   });
 
   describe("API Routes", () => {
     beforeEach(() => {
-      global.fetch = jest.fn().mockImplementation(() => {});
+      global.fetch = jest.fn().mockReturnValue({
+        status: 200,
+      });
     });
 
     afterEach(() => {
@@ -36,84 +22,146 @@ describe("Class: Spotify", () => {
 
     describe("Method: search", () => {
       it("should create request with correct information", async () => {
-        const tokens = {
-          current: {
-            access_token: "foo",
-            refresh_token: "bar",
-          },
-        };
-        const client = Spotify.init(tokens);
         const data = { track: "come and play in the milky night" };
-        await client.search(data);
-        const api = "/api/v1/search";
         const params = {
           headers: { authorization: "foo", "content-type": "application/json" },
           method: "POST",
           body: JSON.stringify(data),
         };
+        const client = Spotify.init();
+        jest.spyOn(client, "_getRequestInitOptions").mockReturnValue(params);
+        await client.search(data);
+        const api = "/api/v1/search";
         expect(global.fetch).toHaveBeenCalledWith(api, params);
       });
     });
 
     describe("Method: getUserId", () => {
       it("should create request with correct information", async () => {
-        const tokens = {
-          current: {
-            access_token: "foo",
-            refresh_token: "bar",
-          },
-        };
-        const client = Spotify.init(tokens);
-        await client.getUserId();
-        const api = "/api/v1/id";
+        const client = Spotify.init();
         const params = {
           headers: { authorization: "foo", "content-type": "application/json" },
           method: "GET",
         };
+        jest.spyOn(client, "_getRequestInitOptions").mockReturnValue(params);
+        await client.getUserId();
+        const api = "/api/v1/id";
+        expect(global.fetch).toHaveBeenCalledWith(api, params);
+      });
+    });
+
+    describe("Method: getGenres", () => {
+      it("should create request with correct information", async () => {
+        const client = Spotify.init();
+        const params = {
+          headers: { authorization: "foo", "content-type": "application/json" },
+          method: "GET",
+        };
+        jest.spyOn(client, "_getRequestInitOptions").mockReturnValue(params);
+        await client.getGenres();
+        const api = "/api/v1/genres";
         expect(global.fetch).toHaveBeenCalledWith(api, params);
       });
     });
 
     describe("Method: createPlaylist", () => {
       it("should create request with correct information", async () => {
-        const tokens = {
-          current: {
-            access_token: "foo",
-            refresh_token: "bar",
-          },
-        };
-        const client = Spotify.init(tokens);
+        const client = Spotify.init();
         const data = { id: "123", name: "test", description: "hello" };
-        await client.createPlaylist(data);
-        const api = "/api/v1/playlist/create";
         const params = {
           headers: { authorization: "foo", "content-type": "application/json" },
           method: "POST",
           body: JSON.stringify(data),
         };
+        jest.spyOn(client, "_getRequestInitOptions").mockReturnValue(params);
+        await client.createPlaylist(data);
+        const api = "/api/v1/playlist/create";
+        expect(global.fetch).toHaveBeenCalledWith(api, params);
+      });
+    });
+
+    describe("Method: getTopItems", () => {
+      it("should create request with correct information", async () => {
+        const client = Spotify.init();
+        const params = {
+          headers: { authorization: "foo", "content-type": "application/json" },
+          method: "GET",
+        };
+        jest.spyOn(client, "_getRequestInitOptions").mockReturnValue(params);
+        await client.getTopItems("artists");
+        const api = "/api/v1/items?type=artists";
         expect(global.fetch).toHaveBeenCalledWith(api, params);
       });
     });
 
     describe("Method: addtoPlaylist", () => {
       it("should create request with correct information", async () => {
-        const tokens = {
-          current: {
-            access_token: "foo",
-            refresh_token: "bar",
-          },
-        };
-        const client = Spotify.init(tokens);
+        const client = Spotify.init();
         const data = { id: "123", uris: ["456"] };
-        await client.addToPlaylist(data);
-        const api = "/api/v1/playlist/add";
         const params = {
           headers: { authorization: "foo", "content-type": "application/json" },
           method: "POST",
           body: JSON.stringify(data),
         };
+        jest.spyOn(client, "_getRequestInitOptions").mockReturnValue(params);
+        await client.addToPlaylist(data);
+        const api = "/api/v1/playlist/add";
         expect(global.fetch).toHaveBeenCalledWith(api, params);
       });
+    });
+
+    describe("Method: getRecommendations", () => {
+      it("should create request with correct information", async () => {
+        const client = Spotify.init();
+        const data = {
+          seedArtists: "3XxNRirzbjfLdDli06zMaB,",
+          seedGenres: "acoustic,breakbeat",
+          seedTracks: "5B6Kjha6RRIMWGN7zGsAaT,6BGNjTZ8zp9MlsIydBa7A9",
+          targetDanceability: 0.7,
+          targetEnergy: 0.4,
+          targetLoudness: 0.3,
+          targetPopularity: 85,
+        };
+        const params = {
+          headers: { authorization: "foo", "content-type": "application/json" },
+          method: "POST",
+          body: JSON.stringify(data),
+        };
+        jest.spyOn(client, "_getRequestInitOptions").mockReturnValue(params);
+        await client.getRecommendations(data);
+        const api = "/api/v1/recommendations";
+        expect(global.fetch).toHaveBeenCalledWith(api, params);
+      });
+    });
+  });
+
+  describe("API Routes: 401 response", () => {
+    beforeEach(() => {
+      global.fetch = jest.fn().mockReturnValue({
+        status: 401,
+      });
+    });
+
+    afterEach(() => {
+      global.fetch.mockClear();
+      delete global.fetch;
+    });
+
+    it("should call resetTokensAndExecute if response returns 401", async () => {
+      const data = { track: "come and play in the milky night" };
+      const params = {
+        headers: { authorization: "foo", "content-type": "application/json" },
+        method: "POST",
+        body: JSON.stringify(data),
+      };
+      const client = Spotify.init();
+      jest.spyOn(client, "_getRequestInitOptions").mockReturnValue(params);
+      jest
+        .spyOn(client, "resetTokensAndExecute")
+        .mockImplementationOnce(jest.fn());
+      await client.search(data);
+      expect(global.fetch).toHaveBeenCalledTimes(1);
+      expect(client.resetTokensAndExecute).toHaveBeenCalledTimes(1);
     });
   });
 });
