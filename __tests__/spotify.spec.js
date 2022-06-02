bc2db50cc27853c93ab985ca2dd2a3c9c3db1054
mhborthwick <mhborthwick@gmail.com>
2022-06-01 22:22:36 -0700

    test: add new spotify api client tests

--- __tests__/spotify.spec.js
+++ __tests__/spotify.spec.js
@@ -50,6 +50,20 @@ describe("Class: Spotify", () => {
       });
     });
 
+    describe("Method: getGenres", () => {
+      it("should create request with correct information", async () => {
+        const client = Spotify.init();
+        const params = {
+          headers: { authorization: "foo", "content-type": "application/json" },
+          method: "GET",
+        };
+        jest.spyOn(client, "_getRequestInitOptions").mockReturnValue(params);
+        await client.getGenres();
+        const api = "/api/v1/genres";
+        expect(global.fetch).toHaveBeenCalledWith(api, params);
+      });
+    });
+
     describe("Method: createPlaylist", () => {
       it("should create request with correct information", async () => {
         const client = Spotify.init();
@@ -66,6 +80,20 @@ describe("Class: Spotify", () => {
       });
     });
 
+    describe("Method: getTopItems", () => {
+      it("should create request with correct information", async () => {
+        const client = Spotify.init();
+        const params = {
+          headers: { authorization: "foo", "content-type": "application/json" },
+          method: "GET",
+        };
+        jest.spyOn(client, "_getRequestInitOptions").mockReturnValue(params);
+        await client.getTopItems("artists");
+        const api = "/api/v1/items?type=artists";
+        expect(global.fetch).toHaveBeenCalledWith(api, params);
+      });
+    });
+
     describe("Method: addtoPlaylist", () => {
       it("should create request with correct information", async () => {
         const client = Spotify.init();
@@ -78,6 +106,30 @@ describe("Class: Spotify", () => {
         jest.spyOn(client, "_getRequestInitOptions").mockReturnValue(params);
         await client.addToPlaylist(data);
         const api = "/api/v1/playlist/add";
+        expect(global.fetch).toHaveBeenCalledWith(api, params);
+      });
+    });
+
+    describe("Method: getRecommendations", () => {
+      it("should create request with correct information", async () => {
+        const client = Spotify.init();
+        const data = {
+          seedArtists: "3XxNRirzbjfLdDli06zMaB,",
+          seedGenres: "acoustic,breakbeat",
+          seedTracks: "5B6Kjha6RRIMWGN7zGsAaT,6BGNjTZ8zp9MlsIydBa7A9",
+          targetDanceability: 0.7,
+          targetEnergy: 0.4,
+          targetLoudness: 0.3,
+          targetPopularity: 85,
+        };
+        const params = {
+          headers: { authorization: "foo", "content-type": "application/json" },
+          method: "POST",
+          body: JSON.stringify(data),
+        };
+        jest.spyOn(client, "_getRequestInitOptions").mockReturnValue(params);
+        await client.getRecommendations(data);
+        const api = "/api/v1/recommendations";
         expect(global.fetch).toHaveBeenCalledWith(api, params);
       });
     });
